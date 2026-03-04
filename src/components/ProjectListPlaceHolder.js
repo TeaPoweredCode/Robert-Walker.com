9c9d5d2afd277499ba9f21a6707d10eaf57c9dbf
Robert Walker <email@Robert-Walker.com>
2026-03-04 22:03:34 +0000

    Deleted Gatsby project
    Deleting everything ready for rewrite

--- src/components/ProjectListPlaceHolder.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import * as styles from '../styles/projectitem.module.css'
-
-export default function ProjectListPlaceHolder() {
-  return (
-    <div className={styles.gridItem}></div>
-  )
-}
